121d0d26a594388c3966f55551656d18fdd3373c
bomin8319 <bomin8319>
2017-08-19 16:22:12 -0400

    simulation edited

--- simulation/BayesSpatialCureSim.R
+++ simulation/BayesSpatialCureSim.R
@@ -41,7 +41,7 @@ set.seed(3)
 n<-1000
 
 #set the number of simulations, and create matrices to store the results
-nsims<-10
+nsims<-1
 
 #history matrix for true estimates
 tru.est<-matrix(NA,nrow=nsims,ncol=8)
@@ -49,17 +49,17 @@ tru.est<-matrix(NA,nrow=nsims,ncol=8)
 tru.est2<-matrix(NA,nrow=nsims,ncol=11)
 
 #history matrix for exp estimates
-exp.est<-matrix(NA,nrow=nsims,ncol=44)
+exp.est<-matrix(NA,nrow=nsims,ncol=46)
 #history matrix for weibull estimates
-weib.est<-matrix(NA,nrow=nsims,ncol=50)
+weib.est<-matrix(NA,nrow=nsims,ncol=52)
 #history matrix for exp RMSE
-exp.rmse<-matrix(NA,nrow=nsims,ncol=22)
+exp.rmse<-matrix(NA,nrow=nsims,ncol=23)
 #history matrix for exp RMSE
-weib.rmse<-matrix(NA,nrow=nsims,ncol=25)
+weib.rmse<-matrix(NA,nrow=nsims,ncol=26)
 #history matrix for exp CP
-exp.cp<-matrix(NA,nrow=nsims,ncol=22)
+exp.cp<-matrix(NA,nrow=nsims,ncol=23)
 #history matrix for exp CP
-weib.cp<-matrix(NA,nrow=nsims,ncol=25)
+weib.cp<-matrix(NA,nrow=nsims,ncol=26)
 
 #create covariates
 x<-runif(n, min=-2.5, max=12)
@@ -86,9 +86,9 @@ tru.est[i,4]<--2
 tru.est[i,5]<-3
 tru.est[i,6]<-1
 
-tru.est2[i,1]<-1   #lambda
-tru.est2[i,2:6]<-w
-tru.est2[i,7:11]<-v
+tru.est2[i,1:5]<-w
+tru.est2[i,6:10]<-v
+tru.est2[i,11]<-1   #lambda
 
 W <- w[s]
 V <- v[s]
@@ -304,6 +304,7 @@ output.SCExponential = summary(mcmc(SCExponential$betas))
 output.SCExponential2 = summary(mcmc(SCExponential$gammas))
 output.SCExponential3 = summary(mcmc(SCExponential$W))
 output.SCExponential4 = summary(mcmc(SCExponential$V))
+output.SCExponential5 = summary(mcmc(SCExponential$lambda))
 
 #store betas and ses
 exp.est[i,15]<-output.SCExponential[[1]][1,1]
@@ -339,6 +340,10 @@ exp.est[i,42]<-output.SCExponential4[[1]][4,4]
 exp.est[i,43]<-output.SCExponential4[[1]][5,1]
 exp.est[i,44]<-output.SCExponential4[[1]][5,4]
 
+exp.est[i,45]<-output.SCExponential5[[1]][1]
+exp.est[i,46]<-output.SCExponential5[[1]][4]
+
+
 #store rmse
 exp.rmse[i,8]<-sqrt((tru.est[i,1]-exp.est[i,15])^2)
 exp.rmse[i,9]<-sqrt((tru.est[i,2]-exp.est[i,17])^2)
@@ -355,6 +360,7 @@ exp.rmse[i,19]<-sqrt((tru.est2[i,7]-exp.est[i,37])^2)
 exp.rmse[i,20]<-sqrt((tru.est2[i,8]-exp.est[i,39])^2)
 exp.rmse[i,21]<-sqrt((tru.est2[i,9]-exp.est[i,41])^2)
 exp.rmse[i,22]<-sqrt((tru.est2[i,10]-exp.est[i,43])^2)
+exp.rmse[i,23]<-sqrt((tru.est2[i,11]-exp.est[i,45])^2)
 
 #calculate upper and lower 95% CI's
 b0.lower<-output.SCExponential[[2]][1,1]
@@ -390,7 +396,8 @@ v4.upper<-output.SCExponential4[[2]][4,5]
 v5.lower<-output.SCExponential4[[2]][5,1]
 v5.upper<-output.SCExponential4[[2]][5,5]
 
-
+l1.lower<-output.SCExponential5[[2]][1]
+l1.upper<-output.SCExponential5[[2]][5]
 #store coverage parameters
 exp.cp[i,8]<-ifelse(tru.est[i,1]>b0.lower & tru.est[i,1]<b0.upper, 1,0)
 exp.cp[i,9]<-ifelse(tru.est[i,2]>b1.lower & tru.est[i,2]<b1.upper, 1,0)
@@ -408,6 +415,7 @@ exp.cp[i,19]<-ifelse(tru.est2[i,7]>v2.lower & tru.est2[i,7]<v2.upper, 1,0)
 exp.cp[i,20]<-ifelse(tru.est2[i,8]>v3.lower & tru.est2[i,8]<v3.upper, 1,0)
 exp.cp[i,21]<-ifelse(tru.est2[i,9]>v4.lower & tru.est2[i,9]<v4.upper, 1,0)
 exp.cp[i,22]<-ifelse(tru.est2[i,10]>v5.lower & tru.est2[i,10]<v5.upper, 1,0)
+exp.cp[i,22]<-ifelse(tru.est2[i,11]>l1.lower & tru.est2[i,11]<l1.upper, 1,0)
 
 
 #####################################################################################
@@ -426,6 +434,7 @@ output.SCWeibull2 = summary(mcmc(SCWeibull$gammas))
 output.SCWeibull3 = summary(mcmc(SCWeibull$rho))
 output.SCWeibull4 = summary(mcmc(SCWeibull$W))
 output.SCWeibull5 = summary(mcmc(SCWeibull$V))
+output.SCWeibull6 = summary(mcmc(SCWeibull$lambda))
 
 #store betas and ses
 weib.est[i,19]<-output.SCWeibull[[1]][1,1]
@@ -463,6 +472,9 @@ weib.est[i,48]<-output.SCWeibull5[[1]][4,4]
 weib.est[i,49]<-output.SCWeibull5[[1]][5,1]
 weib.est[i,50]<-output.SCWeibull5[[1]][5,4]
 
+weib.est[i,51]<-output.SCWeibull6[[1]][1]
+weib.est[i,52]<-output.SCWeibull6[[1]][4]
+
 #store rmse
 weib.rmse[i,10]<-sqrt((tru.est[i,1]-weib.est[i,19])^2)
 weib.rmse[i,11]<-sqrt((tru.est[i,2]-weib.est[i,21])^2)
@@ -480,6 +492,7 @@ weib.rmse[i,22]<-sqrt((tru.est2[i,7]-weib.est[i,43])^2)
 weib.rmse[i,23]<-sqrt((tru.est2[i,8]-weib.est[i,45])^2)
 weib.rmse[i,24]<-sqrt((tru.est2[i,9]-weib.est[i,47])^2)
 weib.rmse[i,25]<-sqrt((tru.est2[i,10]-weib.est[i,49])^2)
+weib.rmse[i,26]<-sqrt((tru.est2[i,11]-weib.est[i,51])^2)
 
 #calculate upper and lower 95% CI's
 b0.lower<-output.SCWeibull[[2]][1,1]
@@ -517,6 +530,9 @@ v4.upper<-output.SCWeibull5[[2]][4,5]
 v5.lower<-output.SCWeibull5[[2]][5,1]
 v5.upper<-output.SCWeibull5[[2]][5,5]
 
+l1.lower<-output.SCWeibull6[[2]][1]
+l1.upper<-output.SCWeibull6[[2]][5]
+
 #store coverage parameters
 weib.cp[i,10]<-ifelse(tru.est[i,1]>b0.lower & tru.est[i,1]<b0.upper, 1,0)
 weib.cp[i,11]<-ifelse(tru.est[i,2]>b1.lower & tru.est[i,2]<b1.upper, 1,0)
@@ -535,6 +551,7 @@ weib.cp[i,22]<-ifelse(tru.est2[i,7]>v2.lower & tru.est2[i,7]<v2.upper, 1,0)
 weib.cp[i,23]<-ifelse(tru.est2[i,8]>v3.lower & tru.est2[i,8]<v3.upper, 1,0)
 weib.cp[i,24]<-ifelse(tru.est2[i,9]>v4.lower & tru.est2[i,9]<v4.upper, 1,0)
 weib.cp[i,25]<-ifelse(tru.est2[i,10]>v5.lower & tru.est2[i,10]<v5.upper, 1,0)
+weib.cp[i,26]<-ifelse(tru.est2[i,11]>l1.lower & tru.est2[i,1]<l1.upper, 1,0)
 
 }
 #combine matrices and label variables
@@ -543,35 +560,31 @@ colnames(main.data)<-c("true.x0","true.x1","true.z0","true.z1","true.z2","true.p
 	"exp.x0","exp.x0.se","exp.x1","exp.x1.se",
 	"cexp.x0","cexp.x0.se","cexp.x1","cexp.x1.se","cexp.z0","cexp.z0.se","cexp.z1","cexp.z1.se","cexp.z2","cexp.z2.se",
 	"scexp.x0","scexp.x0.se","scexp.x1","scexp.x1.se","scexp.z0","scexp.z0.se","scexp.z1","scexp.z1.se","scexp.z2","scexp.z2.se",
-	"scexp.l","scexp.l.se",
 	"scexp.w1","scexp.w1.se","scexp.w2","scexp.w2.se","scexp.w3","scexp.w3.se","scexp.w4","scexp.w4.se","scexp.w5","scexp.w5.se",
 	"scexp.v1","scexp.v1.se","scexp.v2","scexp.v2.se","scexp.v3","scexp.v3.se","scexp.v4","scexp.v4.se","scexp.v5","scexp.v5.se",
-	
+	"scexp.l","scexp.l.se",
 	"wei.x0","wei.x0.se","wei.x1","wei.x1.se","wei.p","wei.p.se",
 	"cwei.x0","cwei.x0.se","cwei.x1","cwei.x1.se","cwei.z0","cwei.z0.se","cwei.z1","cwei.z1.se","cwei.z2","cwei.z2.se","cwei.p","cwei.p.se",
 	"scwei.x0","scwei.x0.se","scwei.x1","scwei.x1.se","scwei.z0","scwei.z0.se","scwei.z1","scwei.z1.se","scwei.z2","scwei.z2.se","scwei.p","scwei.p.se",
-	"scwei.l","scwei.l.se",
 	"scwei.w1","scwei.w1.se","scwei.w2","scwei.w2.se","scwei.w3","scwei.w3.se","scwei.w4","scwei.w4.se","scwei.w5","scwei.w5.se",
 	"scwei.v1","scwei.v1.se","scwei.v2","scwei.v2.se","scwei.v3","scwei.v3.se","scwei.v4","scwei.v4.se","scwei.v5","scwei.v5.se",
-
+	"scwei.l","scwei.l.se",
 	"exp.x0.rmse","exp.x1.rmse","cexp.x0.rmse","cexp.x1.rmse","cexp.z0.rmse","cexp.z1.rmse","cexp.z2.rmse",
 	"scexp.x0.rmse","scexp.x1.rmse","scexp.z0.rmse","scexp.z1.rmse","scexp.z2.rmse",
-	"scexp.l.rmse",
 	"scexp.w1.rmse","scexp.w2.rmse","scexp.w3.rmse","scexp.w4.rmse","scexp.w5.rmse",
 	"scexp.v1.rmse","scexp.v2.rmse","scexp.v3.rmse","scexp.v4.rmse","scexp.v5.rmse",
-	
+	"scexp.l.rmse",
 	"wei.x0.rmse","wei.x1.rmse","wei.p.rmse","cwei.x0.rmse","cwei.x1.rmse","cwei.z0.rmse","cwei.z1.rmse","cwei.z2.rmse","cwei.p.rmse",
 	"scwei.x0.rmse","scwei.x1.rmse","scwei.z0.rmse","scwei.z1.rmse","scwei.z2.rmse","scwei.p.rmse",
-	"scwei.l.rmse",
 	"scwei.w1.rmse","scwei.w2.rmse","scwei.w3.rmse","scwei.w4.rmse","scwei.w5.rmse",
 	"scwei.v1.rmse","scwei.v2.rmse","scwei.v3.rmse","scwei.v4.rmse","scwei.v5.rmse",
-	
+	"scwei.l.rmse",	
 	"exp.x0.cp","exp.x1.cp","cexp.x0.cp","cexp.x1.cp","cexp.z0.cp","cexp.z1.cp","cexp.z2.cp",
 	"wei.x0.cp","wei.x1.cp","wei.p.cp", "cwei.x0.cp","cwei.x1.cp","cwei.z0.cp","cwei.z1.cp","cwei.z2.cp","cwei.p.cp",
 	"scwei.x0.cp","scwei.x1.cp","scwei.z0.cp","scwei.z1.cp","scwei.z2.cp","scwei.p.cp",
-	"scwei.l.cp",
 	"scwei.w1.cp","scwei.w2.cp","scwei.w3.cp","scwei.w4.cp","scwei.w5.cp",
-	"scwei.v1.cp","scwei.v2.cp","scwei.v3.cp","scwei.v4.cp","scwei.v5.cp"
+	"scwei.v1.cp","scwei.v2.cp","scwei.v3.cp","scwei.v4.cp","scwei.v5.cp",
+	"scwei.l.cp",
 	)
 
 #save dataset

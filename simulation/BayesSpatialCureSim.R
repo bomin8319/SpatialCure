0038ec99f4c004ea5f002d6fed29a5b24a638946
bomin8319 <bomin8319>
2017-08-28 17:38:44 -0400

    try building pkg

--- simulation/BayesSpatialCureSim.R
+++ simulation/BayesSpatialCureSim.R
@@ -23,7 +23,6 @@
   library(msm)
   library(verification)
   library(corpcor)
-  library(Design)
   library(SpatialCure)
   library(MCMCpack)
   #set working directory
@@ -41,7 +40,7 @@
   n<-1000
   
   #set the number of simulations, and create matrices to store the results
-  nsims<-1
+  nsims<-1000
   
   #history matrix for true estimates
   tru.est<-matrix(NA,nrow=nsims,ncol=8)
